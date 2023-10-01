3cb02ebc045e75441265e77e8f6037ed47c793e0
alexander <alexandernyaga1999@gmail.com>
2023-10-01 21:34:44 +0300

    add fetch function

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+function fetchDbJson() {
+  // Replace 'db.json' with the actual URL of your JSON file if it's hosted remotely.
+  const url = 'db.json';
+
+  return fetch("http://localhost:3000/characters")
+    .then(response => {
+      // Check if the response status is OK (200)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from ${url}`);
+      }
+      // Parse the JSON response and return it as a JavaScript object
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 const animalList = document.getElementById("animal-list");
+const animalImage = document.getElementById('animal-image');
 const animalVotes = document.getElementById("animal-image");
 const animalButton = document.getElementById("animal-votes");
+
 
 //populate animal list
 
@@ -14,8 +33,8 @@ animalData.forEach(animal => {
 
 //display animal details
 function showAnimalDetails(animal) {
-  animalVotes,textContent = `Votes: ${animal.votes}`};
-
+  animalImage.src = animal.image;
+  animalVotes.textContent = `Votes: ${animal.votes}`};
   voteButton.addEventListener("click", () => voteForAnimal(animal));
 
 
@@ -27,4 +46,5 @@ function showAnimalDetails(animal) {
 
 // Initially show details for the first animal
 showAnimalDetails(animalData[0]);
+
 
