e2e4dce27c35906a8d444e15d19b5895a10f9b6e
alexander <alexandernyaga1999@gmail.com>
2023-10-01 20:18:36 +0300

    populate animal details

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const animalList = document.getElementById("animal-list");
 const animalVotes = document.getElementById("animal-image");
 const animalButton = document.getElementById("animal-votes");
 
+//populate animal list
 
+animalData.forEach(animal => {
+  const listItem = document.createElement("li");
+  listItem.textContent = animal.name;
+  listItem.addEventListener("click", () => showAnimalDeatils(animal));
+  animalList.appendChild(listItem);
+      
+});
