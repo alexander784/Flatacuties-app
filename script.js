7f42d7431fcc3b1ba65cca4ba2cf6af3a9cae76b
alexander <alexandernyaga1999@gmail.com>
2023-10-02 02:33:25 +0300

    add animage image tag

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-function fetchDbJson() {
-  // Replace 'db.json' with the actual URL of your JSON file if it's hosted remotely.
-  const url = 'db.json';
-
-  return fetch("http://localhost:3000/characters")
-    .then(response => {
-      // Check if the response status is OK (200)
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data from ${url}`);
-      }
-      // Parse the JSON response and return it as a JavaScript object
-      return response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
 const animalList = document.getElementById("animal-list");
 const animalImage = document.getElementById('animal-image');
-const animalVotes = document.getElementById("animal-image");
-const animalButton = document.getElementById("animal-votes");
+const animalVotes = document.getElementById("animal-votes");
+const voteButton = document.getElementById("vote-button");
+const apiUrl = 'http://localhost:3000/characters';
 
+let currentAnimal = null; // To store the currently displayed animal
 
-//populate animal list
-
-animalData.forEach(animal => {
-  const listItem = document.createElement("li");
-  listItem.textContent = animal.name;
-  listItem.addEventListener("click", () => showAnimalDeatils(animal));
-  animalList.appendChild(listItem);
-      
-});
-
-//display animal details
-function showAnimalDetails(animal) {
-  animalImage.src = animal.image;
-  animalVotes.textContent = `Votes: ${animal.votes}`};
-  voteButton.addEventListener("click", () => voteForAnimal(animal));
-
-
-  //vote animal
-  function voteForAnimal(animal) {
-    animal.votes++;
-    animalVotes.textContent = `Votes: ${animal.votes}`;
+// Fetch animal data from the local API
+async function fetchAnimalData() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
 }
 
-// Initially show details for the first animal
-showAnimalDetails(animalData[0]);
+// Populate the list of animals
+async function populateAnimalList() {
+    const animalData = await fetchAnimalData();
+    animalData.forEach(animal => {
+        const listItem = document.createElement("li");
+        listItem.textContent = animal.name;
+        listItem.addEventListener("click", () => showAnimalDetails(animal));
+        animalList.appendChild(listItem);
+    });
+}
 
+// Display animal details
+function showAnimalDetails(animal) {
+    currentAnimal = animal;
+    animalImage.src = animal.image;
+    animalVotes.textContent = `Votes: ${animal.votes}`;
+    voteButton.addEventListener("click", voteForAnimal);
+}
 
+// Vote for an animal
+function voteForAnimal() {
+    if (currentAnimal) {
+        currentAnimal.votes++;
+        animalVotes.textContent = `Votes: ${currentAnimal.votes}`;
+    }
+}
+
+// Initially populate the list of animals
+populateAnimalList();
